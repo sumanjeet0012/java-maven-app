cafd613e4d4e605fb1d64baf02f6fdb5d863ef91
sumanjeet0012 <78401409+sumanjeet0012@users.noreply.github.com>
2023-05-03 14:21:14 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,13 +5,13 @@ def buildJar(){
 
 def buildImage(){
    echo "building the docker image"
-   withCredentials([usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER' )])
+   withCredentials([usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER' )]){
    sh "docker build -t sumanjeet0012/demo-app:2.0 ."
 //    sh "echo $PASS | docker login -u $USER --password-stdin"
 //    sh "docker push sumanjeet0012/demo-app:2.0"
                 
 }
-
+}
 def deploy(){
     echo "deploying the image"
 }
