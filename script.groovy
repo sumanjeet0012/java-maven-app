3e747a91bd8d3ca2fda7415edc40b223e486366a
sumanjeet0012 <78401409+sumanjeet0012@users.noreply.github.com>
2023-05-03 11:17:08 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildJar(){
+    echo "building the application"
+    sh "mvn package"
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
+def buildImage(){
+   echo "building the docker image"
+   withCredentials([usernamePassword(credentialsId: "deocker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER' )])
+   sh "docker build -t sumanjeet0012/demo-app:2.0 ."
+   sh "echo $PASS | docker login -u $USER --password-stdin"
+   sh "docker push sumanjeet0012/demo-app:2.0"
+                
+}
 
-def deployApp() {
-    echo 'deploying the application...'
-} 
+def deploy(){
+    echo "deploying the image"
+}
 
 return this
