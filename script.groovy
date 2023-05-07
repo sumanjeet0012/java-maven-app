6054be61989a72590199b65d230df81236b2cbf3
sumanjeet0012 <78401409+sumanjeet0012@users.noreply.github.com>
2023-05-07 13:18:52 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,21 @@
 def buildJar(){
     echo "building the application"
-    sh "mvn package"
 }
 
 def buildImage(){
    echo "building the docker image"
-   withCredentials([usernamePassword(credentialsId: "github", passwordVariable: 'PASS', usernameVariable: 'USER' )]){
-   sh "docker build -t sumanjeet0012/demo-app:2.0 ."
-   sh "echo $PASS | docker login -u $USER --password-stdin"
-   sh "docker push sumanjeet0012/demo-app:2.0"             
+//   withCredentials([usernamePassword(credentialsId: "github", passwordVariable: 'PASS', usernameVariable: 'USER' )]){
+//   sh "docker build -t sumanjeet0012/demo-app:2.0 ."
+//   sh "echo $PASS | docker login -u $USER --password-stdin"
+//   sh "docker push sumanjeet0012/demo-app:2.0"             
    }
 }
 
 def deploy(){
-    echo "deploying the image"
+    def dockerCmd =  "docker run -d -p 3080:80 sumanjeet0012/react-node-example:1.0"
+    sshagent(['ec2-server-key']) {
+    ssh "ssh -0 StrictHostKeyChecking=no ec2-user@54.253.177.19 ${dockerCmd}"
+ }
 }
 
 return this
